4ae72164c34eb2f0d13544ce6c993fe6ca3e8151
XMV22 <cosmic.guy42@gmail.com>
2026-08-29 18:20:08 +0200

    Modifi Xor-C for a crackmes

--- C_Program/Xor-C.c
+++ C_Program/Xor-C.c
@@ -3,19 +3,23 @@
 
 void main() 
 {
-    char key[] = "hi";
-    char msg[] = "msghi_starling";
+    char key[] = {0x04, 0x4F, 0x81, 0xAB, 0xFE, 0x7B, 0xE0, 0xCC, 0x46, 0x35, 0xEE};
+    char msg[] = "AAAAAA";
 
+int scr = 0;
+    
     int keylen = strlen(key);
     int msglen = strlen(msg);
     
     for (int i=0;i<msglen;i++) 
     {
-        msg[i] = msg[i] ^ key[i % keylen];
-    } printf("Encrypt: %s", msg);
+        scr = scr + (msg[i] ^ key[i]);
+    } 
+    printf("Encrypt: %s", msg);
+    printf("\nScore for the crackme: %d", scr);
 
-    for (int i=0;i<msglen;i++) 
-    {
-        msg[i] = msg[i] ^ key[i % keylen];
-    } printf("\nDecrypt: %s", msg);
+
+
+
+
 }
